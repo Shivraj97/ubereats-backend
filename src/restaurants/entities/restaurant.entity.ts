387f6d8de588362b16c42f007fa277e4fec891df
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2021-08-28 19:16:05 +0530

    initial poc completed

---
+++ src/restaurants/entities/restaurant.entity.ts
@@ -0,0 +1,38 @@
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsOptional, IsString, Length } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@ObjectType()
+@Entity()
+export class Restaurant {
+  @Field(() => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
+
+  @Field(() => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsBoolean()
+  @IsOptional()
+  isVegan: boolean;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  address: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  ownersName: string;
+
+  @Field(() => String)
+  @Column()
+  @IsString()
+  categoryName: string;
+}
---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,41 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
+
+@Resolver((of) => Restaurant)
+export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
+  }
+
+  @Mutation(() => Boolean)
+  async createRestaurant(
+    @Args('input') createRestaurantDto: CreateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.createRestaurant(createRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async updateRestaurant(
+    @Args('input') updateRestaurantDto: UpdateRestaurantDto,
+  ): Promise<boolean> {
+    try {
+      await this.restaurantService.updateRestaurant(updateRestaurantDto);
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+}
